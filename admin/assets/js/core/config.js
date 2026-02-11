be8a751eb6459b86268f3f59089dbaf2d697a7e8
usetoffana-coder <usetoffana@gmail.com>
2026-02-10 21:32:11 -0300

    Update config.js

--- admin/assets/js/core/config.js
+++ admin/assets/js/core/config.js
@@ -5,18 +5,18 @@
 
 const AppConfig = {
   firebase: {
-  apiKey: "AIzaSyAZ90xAptvBeRDFl52FI7-WJJD14I6YKk4",
-  authDomain: "drak-feet-admin.firebaseapp.com",
-  projectId: "drak-feet-admin",
-  storageBucket: "drak-feet-admin.firebasestorage.app",
-  messagingSenderId: "525123704162",
-  appId: "1:525123704162:web:ea0a3bc86663334a79c83b",
-  measurementId: "G-EFERT7RZZ5"
+ apiKey: "AIzaSyDXhM8CntfcjNL2sH3vv7DPKDLh3y_Cc4U",
+  authDomain: "toffana-catalogo-97b4d.firebaseapp.com",
+  projectId: "toffana-catalogo-97b4d",
+  storageBucket: "toffana-catalogo-97b4d.firebasestorage.app",
+  messagingSenderId: "919713222744",
+  appId: "1:919713222744:web:07cdbda97e04385cc87a2d",
+  measurementId: "G-W8PKZFEZGR"
   },
 
   cloudinary: {
-   cloudName: 'dz2alj2st',
-    uploadPreset: 'drakfeet_products'
+   cloudName: 'djcx6aick',
+    uploadPreset: 'toffana-catalogo'
   },
 
   appCheck: {
